973ab488971e8c95878ed0b1a1f41ae0fca183b2
ghzhu3 <ghzhu3@iflytek.com>
2018-07-01 23:26:30 +0800

    version 1

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by ghzhu3 on 2018/7/1.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("version");
+    }
+}
